f5d42b61a66fa1bb40febf62fc1a3b8aaac3e1de
skeetybeefy <ko31channel@gmail.com>
2022-08-22 22:53:05 +0300

    Changed port to 80

--- front.js
+++ front.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const path = require("path")
 
-const PORT = process.env.PORT_FRONT || 8080
+const PORT = process.env.PORT_FRONT || 80
 
 const app = express()
 app.use(express.static(__dirname))
